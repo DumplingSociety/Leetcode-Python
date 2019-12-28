24ca27232665d43f75d36252966ec646b77ff17d
DumplingSociety <jobs4ever@me.com>
2019-12-27 19:06:32 -0600

    added more exercises

--- Lists.py
+++ Lists.py
@@ -276,5 +276,73 @@ for line in fhand:
     word = line.split()
     if word in inp_list: continue
     inp_list.append(word)
-print(sorted(inp_list))    
+#print(sorted(inp_list))    
      
+
+# Exercise  8.5: Write a program to read through the mail box data and when you
+# find the line that starts with "From", you will split the line into words
+# using the split function. We are interested in who sent the message, which is
+# second word on the From line.
+# From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008
+# You will parse the From line and print out the second word for each From line,
+# then you will also count the number of From (not From:) lines and print out a
+# count at the end.
+# This is a good sample output with a few lines removed:
+'''
+python fromcount.py
+Enter a file name: mbox-short.txt
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+zqian@umich.edu
+
+[... some output removed...]
+
+ray@media.berkeley.edu
+cwen@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+There were 27 lines in the file with From as the first word
+'''
+count = 0
+fhand = open('mbox-short.txt')
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From': continue
+    print(words[1])
+    count += 1
+print('There were %d lines in the file with From as the first word' % count)
+
+
+"""
+Exercise  8.6: Rewrite the program that prompts the user for a list of numbers
+and prints out the maximum and minimum of the numbers at the end when the user
+enters "done". Write the program to store the numbers the user enters in a list
+and use the max() and min() functions to compute the maximum and minimum
+numbers after the loop completes.
+Enter a number: 6
+Enter a number: 2
+Enter a number: 9
+Enter a number: 3
+Enter a number: 5
+Enter a number: done
+Maximum: 9.0
+Minimum: 2.0
+"""
+
+numlist = list()
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done':
+        break
+    try:
+        value = float(inp)
+    except ValueError:
+        print('Invalid input')
+        quit()
+        
+    numlist.append(value)
+    maxnum = max(numlist)
+    minnum = min(numlist)
+print('Maximum: ', maxnum)
+print('Minimum: ', minnum)
+
